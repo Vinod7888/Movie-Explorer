bfc44453003cccbfacc846008f30ac2246fe27f8
vinod prajapat <vinodprajapat7888@gmail.com>
2025-11-17 10:58:20 +0530

    add

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,4 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-
 export default defineConfig({
-  base: '/Movie-Explorer/',   
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  base: "/Movie-Explorer/",
+  plugins: [tailwindcss()],
+});

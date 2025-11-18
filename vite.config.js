2b2d2d6a8012ff907f36a1cab14c40545edd98c8
vinod prajapat <vinodprajapat7888@gmail.com>
2025-11-18 09:42:40 +0530

    fix: updated files

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
-import React from 'react'
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Movie-Explorer"
-
+  base: "/movie-explorel/", // yaha repo ka naam
 })
